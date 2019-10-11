11dff0ef7643ce9a5cdcfab367330a91487dbdd6
Elias F <32647755+ef4203@users.noreply.github.com>
2019-10-11 18:18:24 +0200

    Fixed build issue

--- Contonso.API.Data/Migrations/20191008154904_InitialMigration.Designer.cs
+++ Contonso.API.Data/Migrations/20191008154904_InitialMigration.Designer.cs
@@ -13,6 +13,10 @@ namespace Contonso.API.Data.Migrations
     [Migration("20191008154904_InitialMigration")]
     partial class InitialMigration
     {
+        /// <summary>
+        /// Implemented to builds the <see cref="P:Microsoft.EntityFrameworkCore.Migrations.Migration.TargetModel" />.
+        /// </summary>
+        /// <param name="modelBuilder">The <see cref="T:Microsoft.EntityFrameworkCore.ModelBuilder" /> to use to build the model.</param>
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
